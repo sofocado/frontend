c1c99c157d223096cb6d61238d03ea4223544f68
Ayauka2003 <ayaulym.parmash79@gmail.com>
2024-05-09 06:47:03 +0500

    Last banners

--- src/views/BannerList.vue
+++ src/views/BannerList.vue
@@ -1,33 +1,52 @@
 <template>
   <div class="all">
-    <a-page-header title="Banner"></a-page-header>
-    <div v-if="info.path == ''" :key="info.name">
+    <a-page-header title="Banner"> <a-button class="button" @click="handleAdd">Add</a-button></a-page-header>
+         
+    <div v-if="count == 0">
       <h1>You don't have a banner! Click this button to add a new banner</h1>
-      <a-button class="button" @click="handleAdd">Add</a-button>
     </div>
 
-    <div v-if="info.path != ''" :key="info.name">
-      <a-card hoverable style="width: 40%; margin-left: 1.9em">
-        <template #cover>
-          <img :alt="baseURL + info.path" :src="baseURL + info.path" />
-        </template>
-        <template #actions>
-          <a-popconfirm
-            title="Delete"
-            ok-text="Yes"
-            cancel-text="No"
-            @confirm="onDelete()"
-          >
-            <a>Delete</a>
-          </a-popconfirm>
-        </template>
-        <a-card-meta
-          :description="
-            $timeFormat(info.startTime, 1) + '-' + $timeFormat(info.endTime, 1)
-          "
-        >
-        </a-card-meta>
-      </a-card>
+    <div v-else>
+       <div class="grid">
+      <a-row
+        justify="start"
+        class="row-wrapper"
+        style="display: flex; flex-wrap: wrap;"
+      >
+        <a-col v-for="item in info" :key="item.name" :span="7" :offset="1">
+          <a-card
+              hoverable
+              style="
+                width: 100%;
+                height: 80%;
+                margin-bottom: 2em;
+              "
+            >
+              <template #cover>
+                <img :alt="baseURL + item.path" :src="baseURL + item.path" class="photo"/>
+              </template>
+              <template #actions>
+                <a-popconfirm
+                  title="Delete"
+                  ok-text="Yes"
+                  cancel-text="No"
+                  @confirm="onDelete(item.bannerId)"
+                >
+                  <a>Delete</a>
+                </a-popconfirm>
+              </template>
+              <a-card-meta
+                :description="
+                  $timeFormat(item.startTime, 1) +
+                  '-' +
+                  $timeFormat(item.endTime, 1)
+                "
+              >
+              </a-card-meta>
+            </a-card>
+        </a-col>
+      </a-row>
+      </div>
     </div>
   </div>
 </template>
@@ -46,6 +65,7 @@ export default {
         startTime: 0,
         endTime: 0,
       },
+      count: 0,
       baseURL: config.baseURL + "/",
     };
   },
@@ -55,27 +75,24 @@ export default {
   methods: {
     loadData() {
       const rid = localStorage.getItem("rid");
-      const bannerId = "";
-      BannerApi("get", { rid, bannerId })
+      BannerApi("list", { rid })
         .then((res) => {
           if (res.result_code == 0) {
-            this.info = JSON.parse(JSON.stringify(res.data));
-          }
-          else{
-            console.log("error")
+            this.info = JSON.parse(JSON.stringify(res.data.rows));
+            this.count = JSON.parse(JSON.stringify(res.data.recordcount));
+          } else {
+            console.log("error");
           }
         })
-        .catch(() => {
-          this.info.path = "";
-        });
+        .catch(() => {});
     },
     handleAdd() {
       this.$router.push({
         name: "BannerAdd",
       });
     },
-    onDelete() {
-      var bannerId = this.info.bannerId;
+    onDelete(bannerI) {
+      var bannerId = bannerI;
       BannerApi("delete", { bannerId }).then((res) => {
         if (res.result_code == 0) {
           this.loadData();
@@ -91,8 +108,15 @@ export default {
 .all {
   width: 80vw;
 }
+.photo {
+  width: 100%;
+  border-radius: 30px;
+  height: 9em;
+  object-fit: cover;
+  position: relative;
+  display: block;
+}
 .button {
-  margin-top: 2em;
   width: 150px;
   background-color: rgb(221, 127, 48);
   border-radius: 20px;
@@ -113,5 +137,10 @@ export default {
   border-radius: 20px;
   object-fit: cover;
   display: block;
+}
+.grid {
+  width: 100%;
+  margin-top: 0.1em;
+  margin-left: -2em;
 }
 </style>
